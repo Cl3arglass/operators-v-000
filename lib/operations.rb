0b77dea9193e1619d8220ae2e2d0534aac9cb13f
Holden Altaffer <haltaffer@gmail.com>
2018-02-21 06:21:03 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,5 +6,5 @@ end
 
 def not_safe?(speed)
 # speed > 60 || speed < 40 ? speed < 60 && speed > 40 : false
-!(speed < 60 && speed > 40) ? speed > 60 || speed < 40 : true
+speed < 60 && speed > 40 ? speed > 60 || speed < 40 : true
 end
